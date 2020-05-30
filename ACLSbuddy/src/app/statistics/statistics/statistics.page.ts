c158f71b2736839c061a6a3de2d1da0546667f50
Danisauri <danividal86@gmail.com>
2020-05-30 13:26:31 -0700

    get age and gender functions ok

--- ACLSbuddy/src/app/statistics/statistics/statistics.page.ts
+++ ACLSbuddy/src/app/statistics/statistics/statistics.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import { Storage } from '@ionic/storage';
+import { ValueAccessor } from '@ionic/angular/directives/control-value-accessors/value-accessor';
 
 @Component({
   selector: 'app-statistics',
@@ -23,7 +25,9 @@ export class StatisticsPage{
   public national: boolean = true;
   public colorArray: any = [];
 
-  constructor() {}
+  constructor(
+    public storage:Storage,
+  ) {}
 
   ionViewDidEnter() {
     this.createRegistersChart();
@@ -50,11 +54,11 @@ export class StatisticsPage{
     this.colorArray.push('rgb(36,161,89)')
     this.colorArray.push('rgb(1,152,117)')
     this.colorArray.push('rgb(109,136,145)')
-    this.colorArray.push('rgb(52,56,94)')
     this.colorArray.push('rgb(75,119,190)')
     this.colorArray.push('rgb(25,120,212)')
     this.colorArray.push('rgb(0,127,170)')
     this.colorArray.push('rgb(42,122,176)')
+    this.colorArray.push('rgb(52,56,94)')
     this.colorArray.push('rgb(0,128,128)')
     this.colorArray.push('rgb(211,211,211)')
   }
@@ -65,7 +69,6 @@ export class StatisticsPage{
       data: {
         labels: ['Complete', 'Partially/Non complete'],
         datasets: [{
-          label: 'Viewers in millions',
           data: this.getRegistersValues(),
           backgroundColor: this.colorArray, 
           borderColor: 'rgb(38, 194, 129)',
@@ -87,10 +90,19 @@ export class StatisticsPage{
     let age = new Chart(await this.ageChart.nativeElement, {
       type: 'bar',
       data: {
-        labels: this.getAges(),
+        labels: ['20 or less yo', 
+          '21 - 30 yo', 
+          '31 - 40 yo', 
+          '41 - 50 yo', 
+          '51 - 60 yo', 
+          '61 - 70 yo', 
+          '71 - 80 yo', 
+          '81 - 90 yo', 
+          '91+ yo', 
+          'Not specified'],
         datasets: [{
           label: 'Number of patients',
-          data: this.getAgesFrecuency(),
+          data: await this.getAgesFrecuency(),
           backgroundColor: this.colorArray, 
           borderColor: 'rgb(38, 194, 129)',
           borderWidth: 1
@@ -109,9 +121,9 @@ export class StatisticsPage{
     let gender = new Chart(await this.genderChart.nativeElement, {
       type: 'pie',
       data: {
-        labels: ['Femenine', 'Masculine'],
+        labels: ['Femenine', 'Masculine', 'Unespecified'],
         datasets: [{
-          data: this.getGenderFrecuency(),
+          data: await this.getGenderFrecuency(),
           backgroundColor: this.colorArray, 
           borderColor: 'rgb(38, 194, 129)',
           borderWidth: 1
@@ -160,14 +172,82 @@ export class StatisticsPage{
     });
   }
 
-  getAges(){
-    return ['30 years', '40 years', '53 years', '60 years']
+  async getAgesFrecuency(){
+    let yearsarray = [[],[],[],[],[],[],[],[],[],[]]
+    let yearsNotSpecified = []
+    let getvalues = []
+    await this.storage.forEach((value) => {
+      getvalues.push(value.age)
+      if (value.age === '' || value.age === null){
+        yearsNotSpecified.push('')
+      }
+      else{
+        let decValue = Math.trunc(parseInt(value.age)/10)
+        switch(decValue){
+          case 0:
+            yearsarray[0].push(decValue);
+            break;
+          case 1:
+            yearsarray[0].push(decValue);
+            break;
+          case 2:
+            yearsarray[1].push(decValue);
+            break;
+          case 3:
+            yearsarray[2].push(decValue);
+            break;
+          case 4:
+            yearsarray[3].push(decValue);
+            break;
+          case 5:
+            yearsarray[4].push(decValue);
+            break;
+          case 6:
+            yearsarray[5].push(decValue);
+            break;
+          case 7:
+            yearsarray[6].push(decValue);
+            break;
+          case 8:
+            yearsarray[7].push(decValue);
+            break;
+          default:
+            yearsarray[8].push(decValue);
+            break;
+        }
+      }
+    })
+    let valuetoreturn = []
+    valuetoreturn.push(yearsarray[0].length)
+    valuetoreturn.push(yearsarray[1].length)
+    valuetoreturn.push(yearsarray[2].length)
+    valuetoreturn.push(yearsarray[3].length)
+    valuetoreturn.push(yearsarray[4].length)
+    valuetoreturn.push(yearsarray[5].length)
+    valuetoreturn.push(yearsarray[6].length)
+    valuetoreturn.push(yearsarray[7].length)
+    valuetoreturn.push(yearsarray[8].length)
+    valuetoreturn.push(yearsNotSpecified.length)
+    return valuetoreturn
   }
-  getAgesFrecuency(){
-    return[5,5,5,5]
-  }
-  getGenderFrecuency(){
-    return [5,7]
+
+  async getGenderFrecuency(){
+    let men = []
+    let women = []
+    let unespecified = []
+    await this.storage.forEach((value => {
+      if(value.gender === 'female'){
+        women.push(value.gender)
+      }
+      else if(value.gender === 'male'){
+        men.push(value.gender)
+      }
+      else{
+        unespecified.push(value.gender)
+      }
+    }))
+    console.log(unespecified)
+    return [women.length, men.length, unespecified.length]
   }
   getRaceFrecuency(){
     return[1,3,3,6,7]
